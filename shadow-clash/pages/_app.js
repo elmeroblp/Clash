e49c0eb29d3acefce8255e008a270f19529d0d9a
elmeroblp <elmeroblp@gmail.com>
2025-05-01 18:51:43 -0600

    Update _app.js

--- shadow-clash/pages/_app.js
+++ shadow-clash/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </>
